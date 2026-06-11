4b40d2392ff6d9f1bc5f310434ff986f4fb99ca5
Nur Ismailov <nismailovwork@gmail.com>
2026-06-11 10:11:45 +0600

    implemented logic to load page data from firebase adn showing on layout page add styles, install react-quill-new

--- src/pages/LayoutPage/LayoutPage.tsx
+++ src/pages/LayoutPage/LayoutPage.tsx
@@ -1,8 +1,57 @@
-const LayoutPage = () => {
-  return (
-    <div>
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { App, Card, Typography } from 'antd';
+import type { PageData } from "../../types/cms.ts";
+import { cmsApi } from "../../api/firebaseApi.ts";
+import Loader from "../../shared/Loader/Loader.tsx";
 
-    </div>
+import "./LayoutPage.css";
+
+const {Title} = Typography;
+
+const LayoutPage = () => {
+  const {pageName} = useParams<{ pageName: string }>();
+
+  const navigate = useNavigate();
+  const {message} = App.useApp();
+
+  const [pageData, setPageData] = useState<PageData | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!pageName) return;
+
+    const loadContent = async () => {
+      setIsLoading(true);
+      try {
+        const data = await cmsApi.getPageByName(pageName);
+        if (data) {
+          setPageData(data);
+        } else {
+          message.error('Page not found!');
+          navigate('/pages/about');
+        }
+      } catch (e) {
+        console.error(e);
+        message.error('Error loading page content');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    void loadContent();
+  }, [pageName, navigate]);
+
+
+  if (isLoading) return <Loader isLoading={isLoading} />;
+
+  return (
+    <Card className="page-content">
+      <Title level={2}>
+        {pageData?.title}
+      </Title>
+      <div>{pageData?.content}</div>
+    </Card>
   );
 };
 
